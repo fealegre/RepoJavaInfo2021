24d369b9b5a682488ca8ac7032a70a83e29858d8
Fernando Alegre <fealegre@hotmail.com>
2025-04-15 01:33:59 -0300

    -

--- informatorio/ComplLevel1/Compl1.java
+++ informatorio/ComplLevel1/Compl1.java
@@ -9,10 +9,10 @@ public class Compl1 {
     public static void main(String[] args) {        
         String nombre;
 
-        Scanner scan= new Scanner(System.in);
-        System.out.print("Ingrese su nombre:>>");
-        nombre=scan.nextLine();
-        System.out.println("\nHOLA " + nombre);
-        scan.close();
+        try (Scanner scan = new Scanner(System.in)) {
+            System.out.print("Ingrese su nombre:>>");
+            nombre=scan.nextLine();
+            System.out.println("\nHOLA " + nombre);
+        }
     }    
 }
